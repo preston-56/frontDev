1c83d783dc6555ba265e9fc52ea01a1e2149d62d
Preston <prestonosoro56@gmail.com>
2022-10-25 17:03:17 +0300

    feat(profile):added profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import photo from '../assets/preston.jpeg'
+const Profile = () => {
+  return (
+    <div>
+      <div className='profile_img'> 
+        <img id="profile__img" src={photo} alt="profile_img" />
+        <p id='name'>Preston</p>
+        <p id='slack'>@PrestonOsoro</p>
+      </div>
+    </div>
+  )
+};
+
+export default Profile
